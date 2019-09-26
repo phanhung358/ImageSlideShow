f598e2de34710a435fedb100c6058bd2a4543bfe
phanhung358 <phanhung.12a1.pdl@gmail.com>
2019-09-26 20:49:54 +0700

    Init project.

---
+++ Slideshow/Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace Slideshow
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.sildePic = new System.Windows.Forms.PictureBox();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.pic5 = new System.Windows.Forms.PictureBox();
+            this.pic4 = new System.Windows.Forms.PictureBox();
+            this.pic3 = new System.Windows.Forms.PictureBox();
+            this.pic2 = new System.Windows.Forms.PictureBox();
+            this.pic1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.sildePic)).BeginInit();
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pic5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.sildePic);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1027, 546);
+            this.panel2.TabIndex = 1;
+            // 
+            // sildePic
+            // 
+            this.sildePic.BackColor = System.Drawing.SystemColors.Control;
+            this.sildePic.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.sildePic.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.sildePic.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.sildePic.Image = ((System.Drawing.Image)(resources.GetObject("sildePic.Image")));
+            this.sildePic.InitialImage = null;
+            this.sildePic.Location = new System.Drawing.Point(0, 0);
+            this.sildePic.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.sildePic.Name = "sildePic";
+            this.sildePic.Size = new System.Drawing.Size(1027, 546);
+            this.sildePic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.sildePic.TabIndex = 0;
+            this.sildePic.TabStop = false;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.panel3.Controls.Add(this.pic5);
+            this.panel3.Controls.Add(this.pic4);
+            this.panel3.Controls.Add(this.pic3);
+            this.panel3.Controls.Add(this.pic2);
+            this.panel3.Controls.Add(this.pic1);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel3.Location = new System.Drawing.Point(0, 445);
+            this.panel3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(1027, 101);
+            this.panel3.TabIndex = 1;
+            // 
+            // pic5
+            // 
+            this.pic5.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.pic5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pic5.Image = ((System.Drawing.Image)(resources.GetObject("pic5.Image")));
+            this.pic5.Location = new System.Drawing.Point(771, 18);
+            this.pic5.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pic5.Name = "pic5";
+            this.pic5.Size = new System.Drawing.Size(95, 67);
+            this.pic5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pic5.TabIndex = 4;
+            this.pic5.TabStop = false;
+            // 
+            // pic4
+            // 
+            this.pic4.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.pic4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pic4.Image = ((System.Drawing.Image)(resources.GetObject("pic4.Image")));
+            this.pic4.Location = new System.Drawing.Point(612, 18);
+            this.pic4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pic4.Name = "pic4";
+            this.pic4.Size = new System.Drawing.Size(95, 67);
+            this.pic4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pic4.TabIndex = 3;
+            this.pic4.TabStop = false;
+            // 
+            // pic3
+            // 
+            this.pic3.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.pic3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pic3.Image = ((System.Drawing.Image)(resources.GetObject("pic3.Image")));
+            this.pic3.Location = new System.Drawing.Point(463, 18);
+            this.pic3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pic3.Name = "pic3";
+            this.pic3.Size = new System.Drawing.Size(95, 67);
+            this.pic3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pic3.TabIndex = 2;
+            this.pic3.TabStop = false;
+            // 
+            // pic2
+            // 
+            this.pic2.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.pic2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pic2.Image = ((System.Drawing.Image)(resources.GetObject("pic2.Image")));
+            this.pic2.Location = new System.Drawing.Point(312, 18);
+            this.pic2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pic2.Name = "pic2";
+            this.pic2.Size = new System.Drawing.Size(95, 67);
+            this.pic2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pic2.TabIndex = 1;
+            this.pic2.TabStop = false;
+            // 
+            // pic1
+            // 
+            this.pic1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.pic1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pic1.Image = ((System.Drawing.Image)(resources.GetObject("pic1.Image")));
+            this.pic1.Location = new System.Drawing.Point(155, 18);
+            this.pic1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pic1.Name = "pic1";
+            this.pic1.Size = new System.Drawing.Size(95, 67);
+            this.pic1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pic1.TabIndex = 0;
+            this.pic1.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 2200;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ClientSize = new System.Drawing.Size(1027, 546);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.sildePic)).EndInit();
+            this.panel3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pic5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.PictureBox sildePic;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pic5;
+        private System.Windows.Forms.PictureBox pic4;
+        private System.Windows.Forms.PictureBox pic3;
+        private System.Windows.Forms.PictureBox pic2;
+        private System.Windows.Forms.PictureBox pic1;
+    }
+}
+
